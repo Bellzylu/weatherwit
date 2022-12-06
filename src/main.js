4b2ec08704682c66ee4ba207452428c52fbe2ab2
Isabella Cappelen <lutkenisabella@gmail.com>
2022-12-06 11:50:54 +0100

    Api calls for weather data, updates of html elements with real data

--- src/main.js
+++ src/main.js
@@ -10,14 +10,19 @@ const emoji = document.querySelector('.emoji');
 const cloudOutput = document.querySelector('.cloud');
 const humidity = document.querySelector('.humidity');
 const windOutput = document.querySelector('.wind');
-const form = document.querySelector('.locationInput');
+const form = document.getElementById('locationInput');
 const search = document.querySelector('.search');
 const btn = document.querySelector('.submit');
-const cities = document.querySelector('.city');
+const cities = document.querySelectorAll('.city');
+const coord = document.querySelector('.coord');
+
+//Weather API key
+let apiKey = "a72eacb9d82e854fa98860dc2139989e";
 
 //Default city when the page loads
+let cityInput = "Oslo";
 
-let cityInput = "London";
+fetchWeatherData();
 
 //Add click event to each city in the panel
 cities.forEach((city) => {
@@ -26,8 +31,6 @@ cities.forEach((city) => {
         cityInput = e.target.innerHTML;
         /* Function that fetches and displays data from the Weather API*/
         fetchWeatherData();
-        //Fade out app with animation
-        app.style.opacity = "0";
 
     });
 })
@@ -43,9 +46,8 @@ form.addEventListener('submit', (e) => {
         //Call api function
         fetchWeatherData();
         //Remove all text from input field
-        search.value = "0";
-//Fade out app with animation
-app.style.opacity = "0";
+        search.value = "";
+
     }
 //Prevent default behavior
 e.preventDefault();
@@ -55,6 +57,85 @@ e.preventDefault();
     /*Function that returns day of the week from a date */
 
     function weekdays(day, month, year) {
+        const weekday = [
+            "Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday"
+    ];
+    return weekday[new Date (`${day}/${month}/${year}`).getDay()];
+    };
+
+    // Function that fetches and displays data from the weather API
+
+    function fetchWeatherData(){
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`)
+        //Take the data (in JSON format and convert it into regular JS objects
+        .then(response => response.json())
+        .then(data => {
+            //console log data to see avaliable variables
+            console.log(data);
+
+             //Create tempElem that holds the temp in celcius to use later
+            temperatureElem = data.main.temp;
+
+            //Adding temperature and weather condition to page
+            temp.innerHTML = Math.round(temperatureElem) + "&#176";
+            nameOutput.innerHTML = data.name;
+            conditionOutput.innerHTML = data.weather[0].description;
+            
+            //Get correct iconId
+            const iconId = data.weather[0].icon;
+            //URL from API without details
+            let iconURL = "http://openweathermap.org/img/wn/";
+            //Create path with correct info
+            let iconPath = iconURL + iconId + "@2x.png";
+            //Add emoji to emoji img in html
+            emoji.src = iconPath;
+
+
+            //Get the humidity from API and change html attribute to correct data
+            humidity.innerHTML = data.main.humidity + "%";
+            windOutput.innerHTML = Math.round(data.wind.speed) + " km/h";
+            coord.innerHTML = data.coord.lat + ", " + data.coord.lon;
+
+            
+        
+        }); }
+
+
+        function showFahrenheitTemp(event){
+            event.preventDefault();
+            // fetchWeatherData()
+            let ceciusTemp = temperatureElem;
+            let farenheit = (ceciusTemp * 9) /5 + 32 ;
+            let tempElement = document.querySelector(".temp");
+            tempElement.innerHTML = Math.round(farenheit) + "&#176" ;
+        }
+
+      let temperatureElem = null;
+
+    function showCelcius(event) {
+        event.preventDefault();
+         let tempElement = document.querySelector(".temp");
+         tempElement.innerHTML = Math.round(temperatureElem) + "&#176" ;
 
     }
+        //Convert C to F and vise versa
+
+        let farenheitLink = document.querySelector("#farenheit");
+        farenheitLink.addEventListener("click", showFahrenheitTemp);
+
+        let celciusLink = document.querySelector("#celcius");
+        celciusLink.addEventListener("click", showCelcius);
+
+
+
+        
+        
+
+        
 
